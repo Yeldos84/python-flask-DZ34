50998f66cb850282db2cb1e19b7c954c8737ecd5
Yeldos <s.yeldos@yandex.kz>
2024-04-10 16:19:06 +0500

    DZ-33

--- Readme.md
+++ Readme.md
@@ -1,6 +1,1 @@
-dz - Flask
-
-1. rout -> /age/<int:age> if age > 18 -> access is allowed
-
-
-2. Test GitHub!
+Домашнее задание №33: Введение в Веб разработку. Основы Flask
--- app.py
+++ app.py
@@ -1,20 +1,47 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, render_template_string
 
 
 app = Flask(__name__)
 
 
-@app.route("/age/<int:age>")
-def check(age):
-    if age < 18:
-        return "Вы несовершеннолетний!"
-    else:
-        return "Доступ получен!"
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 
-@app.route("/users/<int:id_user>/<username>")
-def answer(username, id_user):
-    return f"Ник: {username}, ID: {id_user}"
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/users")
+def users():
+    html = """<html><body>
+    <h1>Hello!</h1>
+    <h3>Enter user name in browser "/user/user_name"</h3>
+    </body></html>"""
+    return render_template_string(html)
+    # return 'Enter user name in browser "/user/user_name"'
+
+
+users = []
+
+
+@app.route("/users/<user>")
+def add_users(user):
+    users.append(user)
+
+    return render_template("users.html", users=users)
 
 
 if __name__ == "__main__":
